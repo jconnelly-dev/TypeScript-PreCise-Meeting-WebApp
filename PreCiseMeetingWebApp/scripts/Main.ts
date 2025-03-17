88362c19a8ebbe035ae3ec1fad8059a7587d65ba
jconnelly-dev <53546309+jconnelly-dev@users.noreply.github.com>
2025-03-17 17:57:08 -0600

    added copy to clipboard button - nearly there, this some minor bugs

--- PreCiseMeetingWebApp/scripts/Main.ts
+++ PreCiseMeetingWebApp/scripts/Main.ts
@@ -3,12 +3,9 @@ import { FileHelper } from './FileHelper.js';
 import { Logger } from './Logger.js';
 import { Meetings } from './Meetings.js';
 
-const FILE_BUILD_DETAILS: string = '/files/build.txt';
-const FILE_MEETING_NAMES: string = '/files/names.txt';
-
 // Ensure this script runs -> after the HTML document is fully parsed, but before the DOM content is fully loaded and displayed.
 document.addEventListener('DOMContentLoaded', async () => {
-    Logger.log(Logger.LogLevel.INFO, "DOM", addEventListener);
+    Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener);
 
     const userNow: Date = new Date();
 
@@ -21,6 +18,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Set the text content of the <time> element to the utc datetime found in 'BUILD-FILE-NAME' converted to the user's timezone.
     const publishTimeElement: HTMLElement | null = document.getElementById('last-publish-datetime');
     if (publishTimeElement) {
+        const FILE_BUILD_DETAILS: string = '/files/build.txt';
         const publishDate: Date | null = await FileHelper.asyncGetUtcDateFromContents(FILE_BUILD_DETAILS);
         publishTimeElement.textContent = (publishDate) ? DateHelper.formatDateString(publishDate) : "Invalid date";
     }
@@ -30,6 +28,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const listElement = listSectionElement?.querySelector('ul');
     if (listSectionElement && listElement) {
         listElement.innerHTML = ''; // clear existing list items
+        const FILE_MEETING_NAMES: string = '/files/names.txt';
         const names: string[] | null = await FileHelper.asyncGetParticipantsFromContents(FILE_MEETING_NAMES);
         if (names && names.length > 0) {
             const meetingNames: string[] = Meetings.getMeetingForWeek(userNow, names);
@@ -42,7 +41,71 @@ document.addEventListener('DOMContentLoaded', async () => {
                     itemElement.append(` ${name}`);
                     listElement.appendChild(itemElement);
                 });
+
+                // Adding an event listener for the button, now that the DOM has been fully loaded.
+                Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "before getting buttonElement...");
+                const buttonElement: HTMLButtonElement | null = document.getElementById('copy-button') as HTMLButtonElement;
+                Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "after getting buttonElement...");
+                if (buttonElement) {
+                    Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "buttonElement found!");
+                    buttonElement.addEventListener('click', copyElementChangeState);  // Add event listener here
+                }
             }
         }
     }
 });
+
+function copyElementChangeState(): void {
+    const attendees: string[] = extractParticipants();
+    const meetingDate: Date | null = extractMeetingDate();
+    let iconElement: HTMLElement | null = document.getElementById('copy-icon');
+    if (attendees && meetingDate && iconElement) {
+        iconElement.classList.remove('fa-copy');
+        insertNamesIntoClipboard(meetingDate, attendees);
+        iconElement.classList.add('fa-check');
+    }
+}
+
+function extractParticipants(): string[] {
+    let extractedNames: string[] = [];
+    const listSectionElement: HTMLElement | null = document.getElementById('participants-list');
+    if (listSectionElement) {
+        const participantItems = listSectionElement.querySelectorAll('li');
+        if (participantItems) {
+            participantItems.forEach((item) => {
+                // Remove the number and any leading whitespace.
+                const name: string | undefined = item.textContent?.replace(/^\d+\s*/, '').trim();
+                if (name) {
+                    extractedNames.push(name);
+                }
+            });
+        }
+    }
+    return extractedNames;
+}
+
+function extractMeetingDate(): Date | null {
+    let extractedDate: Date | null = null;
+    const dateElement: HTMLElement | null = document.getElementById('day-of-meeting');
+    if (dateElement) {
+        const dateString = dateElement.textContent?.trim();
+        if (dateString) {
+            extractedDate = new Date(dateString);
+        }
+    }
+    return extractedDate;
+}
+
+function insertNamesIntoClipboard(date: Date, attendees: string[]): void {
+    let copiedText: string = "--- START MEETING ---";
+    if (date) {
+        const newLine = (typeof window === "undefined") ? "\r\n" : "\n"; // for Node.js vs browser
+        copiedText += `${newLine}--- ${date} ---`;
+        if (attendees) {
+            attendees.forEach((attendee, idx) => {
+                copiedText += `${newLine}[${idx}] ${attendee}`;
+            });
+        }
+        navigator.clipboard.writeText(copiedText);
+    }
+}
--- PreCiseMeetingWebApp/wwwroot/dist/Main.js
+++ PreCiseMeetingWebApp/wwwroot/dist/Main.js
@@ -11,11 +11,9 @@ import { DateHelper } from './DateHelper.js';
 import { FileHelper } from './FileHelper.js';
 import { Logger } from './Logger.js';
 import { Meetings } from './Meetings.js';
-const FILE_BUILD_DETAILS = '/files/build.txt';
-const FILE_MEETING_NAMES = '/files/names.txt';
 // Ensure this script runs -> after the HTML document is fully parsed, but before the DOM content is fully loaded and displayed.
 document.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, void 0, function* () {
-    Logger.log(Logger.LogLevel.INFO, "DOM", addEventListener);
+    Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener);
     const userNow = new Date();
     // Set the text content of the <time> element to utcNow converted to the user's timezone.
     const meetingTimeElement = document.getElementById('day-of-meeting');
@@ -25,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, vo
     // Set the text content of the <time> element to the utc datetime found in 'BUILD-FILE-NAME' converted to the user's timezone.
     const publishTimeElement = document.getElementById('last-publish-datetime');
     if (publishTimeElement) {
+        const FILE_BUILD_DETAILS = '/files/build.txt';
         const publishDate = yield FileHelper.asyncGetUtcDateFromContents(FILE_BUILD_DETAILS);
         publishTimeElement.textContent = (publishDate) ? DateHelper.formatDateString(publishDate) : "Invalid date";
     }
@@ -33,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, vo
     const listElement = listSectionElement === null || listSectionElement === void 0 ? void 0 : listSectionElement.querySelector('ul');
     if (listSectionElement && listElement) {
         listElement.innerHTML = ''; // clear existing list items
+        const FILE_MEETING_NAMES = '/files/names.txt';
         const names = yield FileHelper.asyncGetParticipantsFromContents(FILE_MEETING_NAMES);
         if (names && names.length > 0) {
             const meetingNames = Meetings.getMeetingForWeek(userNow, names);
@@ -45,7 +45,68 @@ document.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, vo
                     itemElement.append(` ${name}`);
                     listElement.appendChild(itemElement);
                 });
+                // Adding an event listener for the button, now that the DOM has been fully loaded.
+                Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "before getting buttonElement...");
+                const buttonElement = document.getElementById('copy-button');
+                Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "after getting buttonElement...");
+                if (buttonElement) {
+                    Logger.log(Logger.LogLevel.PROD, "DOM", addEventListener, "buttonElement found!");
+                    buttonElement.addEventListener('click', copyElementChangeState); // Add event listener here
+                }
             }
         }
     }
 }));
+function copyElementChangeState() {
+    const attendees = extractParticipants();
+    const meetingDate = extractMeetingDate();
+    let iconElement = document.getElementById('copy-icon');
+    if (attendees && meetingDate && iconElement) {
+        iconElement.classList.remove('fa-copy');
+        insertNamesIntoClipboard(meetingDate, attendees);
+        iconElement.classList.add('fa-check');
+    }
+}
+function extractParticipants() {
+    let extractedNames = [];
+    const listSectionElement = document.getElementById('participants-list');
+    if (listSectionElement) {
+        const participantItems = listSectionElement.querySelectorAll('li');
+        if (participantItems) {
+            participantItems.forEach((item) => {
+                var _a;
+                // Remove the number and any leading whitespace.
+                const name = (_a = item.textContent) === null || _a === void 0 ? void 0 : _a.replace(/^\d+\s*/, '').trim();
+                if (name) {
+                    extractedNames.push(name);
+                }
+            });
+        }
+    }
+    return extractedNames;
+}
+function extractMeetingDate() {
+    var _a;
+    let extractedDate = null;
+    const dateElement = document.getElementById('day-of-meeting');
+    if (dateElement) {
+        const dateString = (_a = dateElement.textContent) === null || _a === void 0 ? void 0 : _a.trim();
+        if (dateString) {
+            extractedDate = new Date(dateString);
+        }
+    }
+    return extractedDate;
+}
+function insertNamesIntoClipboard(date, attendees) {
+    let copiedText = "--- START MEETING ---";
+    if (date) {
+        const newLine = (typeof window === "undefined") ? "\r\n" : "\n"; // for Node.js vs browser
+        copiedText += `${newLine}--- ${date} ---`;
+        if (attendees) {
+            attendees.forEach((attendee, idx) => {
+                copiedText += `${newLine}[${idx}] ${attendee}`;
+            });
+        }
+        navigator.clipboard.writeText(copiedText);
+    }
+}
